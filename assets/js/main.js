7ce5ab423ece7ae022a590b76a2b12d0866ed986
fauzanocthory <fauzanocthory@gmail.com>
2025-08-01 01:15:14 +0700

    Pening Pala

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,109 @@
 * License: https://bootstrapmade.com/license/
 */
 
-(function() {
+// MULTI STEP FORM JS
+let step = document.getElementsByClassName('step');
+let prevBtn = document.getElementById('prev-btn');
+let nextBtn = document.getElementById('next-btn');
+let submitBtn = document.getElementById('submit-btn');
+let form = document.getElementsByTagName('form')[0];
+let preloader = document.getElementById('preloader-wrapper');
+let bodyElement = document.querySelector('body');
+let succcessDiv = document.getElementById('success');
+
+form.onsubmit = () => {
+  return false
+}
+let current_step = 0;
+let stepCount = 18
+step[current_step].classList.add('d-block');
+if (current_step == 0) {
+  prevBtn.classList.add('d-none');
+  submitBtn.classList.add('d-none');
+  nextBtn.classList.add('d-inline-block');
+}
+
+const progress = (value) => {
+  document.getElementsByClassName('progress-bar')[0].style.width = `${value}%`;
+}
+
+nextBtn.addEventListener('click', () => {
+  current_step++;
+  let previous_step = current_step - 1;
+  if ((current_step > 0) && (current_step <= stepCount)) {
+    prevBtn.classList.remove('d-none');
+    prevBtn.classList.add('d-inline-block');
+    step[current_step].classList.remove('d-none');
+    step[current_step].classList.add('d-block');
+    step[previous_step].classList.remove('d-block');
+    step[previous_step].classList.add('d-none');
+    if (current_step == stepCount) {
+      submitBtn.classList.remove('d-none');
+      submitBtn.classList.add('d-inline-block');
+      nextBtn.classList.remove('d-inline-block');
+      nextBtn.classList.add('d-none');
+    }
+  } else {
+    if (current_step > stepCount) {
+      form.onsubmit = () => {
+        return true
+      }
+    }
+  }
+  progress((100 / stepCount) * current_step);
+});
+
+
+prevBtn.addEventListener('click', () => {
+  if (current_step > 0) {
+    current_step--;
+    let previous_step = current_step + 1;
+    prevBtn.classList.add('d-none');
+    prevBtn.classList.add('d-inline-block');
+    step[current_step].classList.remove('d-none');
+    step[current_step].classList.add('d-block')
+    step[previous_step].classList.remove('d-block');
+    step[previous_step].classList.add('d-none');
+    if (current_step < stepCount) {
+      submitBtn.classList.remove('d-inline-block');
+      submitBtn.classList.add('d-none');
+      nextBtn.classList.remove('d-none');
+      nextBtn.classList.add('d-inline-block');
+      prevBtn.classList.remove('d-none');
+      prevBtn.classList.add('d-inline-block');
+    }
+  }
+
+  if (current_step == 0) {
+    prevBtn.classList.remove('d-inline-block');
+    prevBtn.classList.add('d-none');
+  }
+  progress((100 / stepCount) * current_step);
+});
+
+
+submitBtn.addEventListener('click', () => {
+  preloader.classList.add('d-block');
+
+  const timer = ms => new Promise(res => setTimeout(res, ms));
+
+  timer(3000)
+    .then(() => {
+      bodyElement.classList.add('loaded');
+    }).then(() => {
+      step[stepCount].classList.remove('d-block');
+      step[stepCount].classList.add('d-none');
+      prevBtn.classList.remove('d-inline-block');
+      prevBtn.classList.add('d-none');
+      submitBtn.classList.remove('d-inline-block');
+      submitBtn.classList.add('d-none');
+      succcessDiv.classList.remove('d-none');
+      succcessDiv.classList.add('d-block');
+    })
+
+});
+
+(function () {
   "use strict";
 
   /**
@@ -52,7 +154,7 @@
    * Toggle mobile nav dropdowns
    */
   document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
+    navmenu.addEventListener('click', function (e) {
       e.preventDefault();
       this.parentNode.classList.toggle('active');
       this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
@@ -113,7 +215,7 @@
    * Init swiper sliders
    */
   function initSwiper() {
-    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+    document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
       let config = JSON.parse(
         swiperElement.querySelector(".swiper-config").innerHTML.trim()
       );
@@ -134,12 +236,12 @@
 
   const pricingContainers = document.querySelectorAll('.pricing-toggle-container');
 
-  pricingContainers.forEach(function(container) {
+  pricingContainers.forEach(function (container) {
     const pricingSwitch = container.querySelector('.pricing-toggle input[type="checkbox"]');
     const monthlyText = container.querySelector('.monthly');
     const yearlyText = container.querySelector('.yearly');
 
-    pricingSwitch.addEventListener('change', function() {
+    pricingSwitch.addEventListener('change', function () {
       const pricingItems = container.querySelectorAll('.pricing-item');
 
       if (this.checked) {
@@ -170,7 +272,7 @@
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
-  window.addEventListener('load', function(e) {
+  window.addEventListener('load', function (e) {
     if (window.location.hash) {
       if (document.querySelector(window.location.hash)) {
         setTimeout(() => {
